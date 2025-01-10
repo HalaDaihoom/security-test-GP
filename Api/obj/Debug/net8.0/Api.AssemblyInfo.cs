64bbc00c4abb781c1eb69619365e8fbdf91ff5b4
HalaDaihoom <haladaihoom382@gmail.com>
2025-01-09 18:55:39 -0800

    t

--- Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f4384214eba2a0b7b1cc209622a4a0066ca50e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c50e1955eba05cd81e90e3372611a86a266931d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
