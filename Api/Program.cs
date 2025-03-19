14d2ba9494ce9d7418186f996dac62f9fbf3bb93
7ala Daihoom <104699700+HalaDaihoom@users.noreply.github.com>
2025-03-19 13:57:16 +0200

    Update Program.cs

--- Api/Program.cs
+++ Api/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +22,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
 // JWT configuration
 var jwtSettings = new JWT
 {
@@ -71,7 +74,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowSpecificOrigin",
         builder =>
         {
-            builder.WithOrigins("http://localhost:3000") // Your frontend URL
+            builder.WithOrigins("https://scan-website-front2.vercel.app/") // Your frontend URL
                    .AllowAnyHeader()
                    .AllowAnyMethod();
         });
@@ -86,6 +89,8 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 // Database configuration
+
+
 builder.Services.AddDbContext<ApiContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("Api"),
@@ -110,6 +115,11 @@ builder.Services.ConfigureApplicationCookie(options =>
     };
 });
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(8080); // Azure requires port 8080
+});
+
 // Services for authorization
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddHttpClient<ZapService>(); // Ensure this line is here
@@ -123,6 +133,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
 // Apply the CORS policy globally
@@ -135,165 +146,13 @@ app.UseAuthorization();
 // Map controllers
 app.MapControllers();
 
-app.Run();
+try
+{
+    app.Run();
+}
+catch (Exception ex)
+{
+    app.Logger.LogCritical(ex, "Application startup failed!");
+    throw;
+}
 
-
-// // using Microsoft.AspNetCore.Authentication.JwtBearer;
-// // using Microsoft.AspNetCore.Builder;
-// // using Microsoft.AspNetCore.Hosting;
-// // using Microsoft.AspNetCore.Identity;
-// // using Microsoft.EntityFrameworkCore;
-// // using Microsoft.Extensions.Configuration;
-// // using Microsoft.Extensions.DependencyInjection;
-// // using Microsoft.Extensions.Hosting;
-// // using Microsoft.IdentityModel.Tokens;
-// // using Microsoft.OpenApi.Models;
-// // using System.Text;
-// // using System;
-// // using Api.Helpers;
-// // using Api.Models;
-// // using Api.Services;
-
-// // var builder = WebApplication.CreateBuilder(args);
-
-// // // Add services to the container.
-// // builder.Services.AddControllers();
-
-// // //Jwt configuration
-// // builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
-// // builder.Services.AddAuthentication(options =>
-// // {
-// //     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-// //     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-// //     options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme; // Add this line
-// // })
-// // .AddJwtBearer(o =>
-// // {
-// //     o.RequireHttpsMetadata = true;
-// //     o.SaveToken = true;
-
-// //     o.TokenValidationParameters = new TokenValidationParameters
-// //     {
-// //         ValidateIssuer = true,
-// //         ValidateAudience = true,
-// //         ValidateLifetime = true,
-// //         ValidateIssuerSigningKey = true,
-// //         ValidIssuer = builder.Configuration["JWT:Issuer"],
-// //         ValidAudience = builder.Configuration["JWT:Audience"],
-// //         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
-// //     };
-// // });
-
-
-
-
-
-// // // builder.Services.Configure<JWT>(builder.Configuration.GetSection("JWT"));
-// // // builder.Services.AddAuthentication(options => {
-// // //     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-// // //     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-// // // })
-// // // .AddJwtBearer(o =>
-// // // {
-// // //     o.RequireHttpsMetadata = false;
-// // //     o.SaveToken = false;
-// // //     o.TokenValidationParameters = new TokenValidationParameters
-// // //     {
-// // //         ValidateIssuerSigningKey = true,
-// // //         ValidateIssuer = true,
-// // //         ValidateAudience = true,
-// // //         ValidateLifetime = true,
-// // //         ValidIssuer = builder.Configuration["JWT:Issuer"],
-// // //         ValidAudience = builder.Configuration["JWT:Audience"],
-// // //         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
-// // //     };
-
-// // //     // Log any authentication issues
-// // //     o.Events = new JwtBearerEvents
-// // //     {
-// // //         OnAuthenticationFailed = context =>
-// // //         {
-// // //             Console.WriteLine("JWT authentication failed:", context.Exception.Message);
-// // //             return Task.CompletedTask;
-// // //         }
-// // //     };
-// // // });
-
-// // // Identity
-// // builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-// //     .AddEntityFrameworkStores<ApiContext>();
-
-    
-
-// // // Services for authorization
-// // builder.Services.AddScoped<IAuthService, AuthService>();
-
-// // // Add CORS policy
-// // builder.Services.AddCors(options =>
-// // {
-// //     options.AddPolicy("AllowSpecificOrigin",
-// //         builder =>
-// //         {
-// //             builder.WithOrigins("http://localhost:3000") // Your frontend URL
-// //                    .AllowAnyHeader()
-// //                    .AllowAnyMethod();
-// //         });
-// // });
-
-// // // builder.Services.AddCors(options =>
-// // // {
-// // //     options.AddPolicy("AllowSpecificOrigin",
-// // //         builder =>
-// // //         {
-// // //             builder.WithOrigins("http://localhost:3000") // Allow your frontend URL
-// // //                    .AllowAnyHeader()
-// // //                    .AllowAnyMethod();
-// // //         });
-// // // });
-
-// // builder.Services.AddEndpointsApiExplorer();
-// // builder.Services.AddSwaggerGen();
-
-// // // Use builder.Configuration instead of Configuration. (connection string)
-// // builder.Services.AddDbContext<ApiContext>(options =>
-// //     options.UseMySql(
-// //         builder.Configuration.GetConnectionString("Api"),
-// //         ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("Api"))
-// //     )
-// // );
-
-
-// // builder.Services.ConfigureApplicationCookie(options =>
-// // {
-// //     options.Events.OnRedirectToLogin = context =>
-// //     {
-// //         context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-// //         return Task.CompletedTask;
-// //     };
-// // });
-
-
-
-// // var app = builder.Build();
-
-// // // Configure the HTTP request pipeline.
-// // if (app.Environment.IsDevelopment())
-// // {
-// //     app.UseSwagger();
-// //     app.UseSwaggerUI();
-// // }
-
-// // app.UseHttpsRedirection();
-
-// // // Apply the CORS policy globally
-// // app.UseCors("AllowSpecificOrigin");
-
-// // // Ensure this is before UseAuthorization
-// // app.UseAuthentication();
-// // app.UseAuthorization();
-
-
-// // // Map controllers
-// // app.MapControllers();
-
-// // app.Run();
